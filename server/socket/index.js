e4244cc6d2b79129fa14a06f65e2eb7ff20d8999
Udhay Singh Manhas <udhay.manhas@gmail.com>
2016-08-25 12:53:57 +0530

    ui update

--- server/socket/index.js
+++ server/socket/index.js
@@ -5,21 +5,20 @@ var UserDeamon = require('../models/userDeamonModel'); //model for user subscrip
 exports.start_socket = function(io,clients){
 
     io.sockets.on('connection', function(socket){
-        //this is where socket is initialized and it recieved user id and updates info about users/clients and their subscriptions
+        //this is where socket is initialized and it recieves user id and updates info about users/clients and their subscriptions
         socket.on('uid', function(uid){
-            var daemon={};
-            UserDeamon.findOne({"uid":uid},'database',function(err,data){
+            socket.uid = uid;
+            UserDeamon.findOne({"uid":socket.uid},'database',function(err,data){
                 if(err) {console.log('err............');}
                 if(!data) {console.log('No deamon exits............ creating deamon');
-                    clients[uid]={
+                    clients[socket.uid]={
                         "socket":socket,
                         "databse":''
                     };
                 }
                 else{
                     console.log('Client Connected: Verified');
-                    //deamon = data.database;
-                    clients[uid]={
+                    clients[socket.uid]={
                         "socket":socket,
                         "database":data.database
                     };
@@ -27,6 +26,11 @@ exports.start_socket = function(io,clients){
 
             });
 
-        })
+        });
+
+        socket.on('disconnect', function(data){
+            if(!socket.uid) return;
+            delete clients[socket.uid];
+        });
     });
 };
